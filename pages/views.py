ade8642a92e2d594ec993eeb7df642b39d9e9735
lorentrs <lorentr@hotmail.com>
2020-02-15 22:47:25 +0300

    dsfsda

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by("published_date")
+    return render(request,'pages/index.html',{'posts' : posts})
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def post_list(request):
+    pass
