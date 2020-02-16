8b24f0c5b98d2232264b2589edeb2cdc2176ffd1
lorentrs <lorentr@hotmail.com>
2020-02-16 21:42:13 +0300

    mksdafas

--- pages/views.py
+++ pages/views.py
@@ -9,12 +9,6 @@ def index(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by("published_date")
     return render(request,'pages/index.html',{'posts' : posts})
 
-def about(request):
-    return render(request, 'pages/about.html')
-
-def post_list(request):
-    pass
-
-def post_detail(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     return render(request,'pages/post_detail.html',{'post' : post})
